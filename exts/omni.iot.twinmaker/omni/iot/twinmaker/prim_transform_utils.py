3fea767a0c9130f414c6882f6161685a55941fc6
Harry Wandersman <hwanders@amazon.com>
2023-02-06 19:57:22 +0000

    Fixed USD conversion and setting node transforms

--- exts/omni.iot.twinmaker/omni/iot/twinmaker/prim_transform_utils.py
+++ exts/omni.iot.twinmaker/omni/iot/twinmaker/prim_transform_utils.py
@@ -1,4 +1,5 @@
 from pxr import Usd, UsdGeom, Gf
+from scipy.spatial.transform import Rotation
 
 # Credit to https://github.com/ft-lab/Omniverse_extension_SetOrigin/blob/main/exts/ft_lab.Tools.SetOrigin/ft_lab/Tools/SetOrigin/scripts/TransformUtil.py
 
@@ -7,7 +8,8 @@ from pxr import Usd, UsdGeom, Gf
 # Set Translate.
 # ---------------------------.
 def TUtil_SetTranslate(prim: Usd.Prim, vector):
-    tV = Gf.Vec3f(vector[0], vector[1], vector[2])
+    # Referencing a USD enlarges the model by 100x (translations are 100x) when compared to GLTF/GLB
+    tV = Gf.Vec3f(vector[0] * 100, vector[1] * 100, vector[2] * 100)
     trans = prim.GetAttribute("xformOp:translate").Get()
 
     if trans is not None:
@@ -45,25 +47,18 @@ def TUtil_SetScale(prim: Usd.Prim, vector):
 # Set Rotate.
 # ---------------------------.
 def TUtil_SetRotate(prim: Usd.Prim, vector):
-    rV = Gf.Vec3f(vector[0], vector[1], vector[2])
-    # Get rotOrder.
-    # If rotation does not exist, rotOrder = UsdGeom.XformCommonAPI.RotationOrderXYZ.
-    xformAPI = UsdGeom.XformCommonAPI(prim)
-    time_code = Usd.TimeCode.Default()
-    _, _, _, _, rotOrder = xformAPI.GetXformVectors(time_code)
+    # OV rotation calculated in quaternions
+    # TM scene rotations are in XYZ radians
+    rotation = Rotation.from_euler("xyz", vector)
+    rotQuat = rotation.as_quat()
+    rV = Gf.Quatf(rotQuat[3], rotQuat[0], rotQuat[1], rotQuat[2])
+    rot = prim.GetAttribute("xformOp:orient").Get()
 
-    # Convert rotOrder to "xformOp:rotateXYZ" etc.
-    t = xformAPI.ConvertRotationOrderToOpType(rotOrder)
-    rotateAttrName = "xformOp:" + UsdGeom.XformOp.GetOpTypeToken(t)
-
-    # Set rotate.
-    rotate = prim.GetAttribute(rotateAttrName).Get()
-    if rotate is not None:
-        # Specify a value for each type.
-        if type(rotate) == Gf.Vec3f:
-            prim.GetAttribute(rotateAttrName).Set(Gf.Vec3f(rV))
-        elif type(rotate) == Gf.Vec3d:
-            prim.GetAttribute(rotateAttrName).Set(Gf.Vec3d(rV))
+    if rot is not None:
+        if type(rot) == Gf.Quatf:
+            prim.GetAttribute("xformOp:orient").Set(Gf.Quatf(rV))
+        elif type(rot) == Gf.Quatd:
+            prim.GetAttribute("xformOp:orient").Set(Gf.Quatd(rV))
     else:
-        # xformOpOrder is also updated.
-        xformAPI.SetRotate(Gf.Vec3f(rV), rotOrder)
+        xformAPI = UsdGeom.XformCommonAPI(prim)
+        xformAPI.SetRotate(Gf.Quatf(rV))
--- exts/omni.iot.twinmaker/omni/iot/twinmaker/scene_importer.py
+++ exts/omni.iot.twinmaker/omni/iot/twinmaker/scene_importer.py
@@ -53,19 +53,32 @@ class SceneImporter:
     def __import_progress_callback(current_step: int, total: int):
         print(f"{current_step} of {total}")
 
+    # Download 3D model from S3
     async def __load_model(self, modelPath):
-        print('Loading model from S3: {}'.format(modelPath))
-        self._s3Client.download_file(
-            self._workspaceBucket,
-            modelPath,
-            modelPath
-        )
+        modelAlreadyDownloaded = os.path.isfile(modelPath)
+        if not modelAlreadyDownloaded:
+            print('Loading model from S3: {}'.format(modelPath))
+            self._s3Client.download_file(
+                self._workspaceBucket,
+                modelPath,
+                modelPath
+            )
 
+    def __convert_file_name(self, filePath, format):
+        fileName = filePath.split('.')[-2]
+        return '{}\\{}.{}'.format(os.getcwd(), fileName, format)
+
+    # Convert model to USD
+    async def __convert_to_usd(self, modelPath):
         task_manager = converter.get_instance()
-        path = '.'
+        outputPath = self.__convert_file_name(modelPath, 'usd')
+        modelAlreadyImported = os.path.isfile(outputPath)
+        if modelAlreadyImported:
+            return outputPath
+
         task = task_manager.create_converter_task(
-            path,
-            path,
+            modelPath,
+            outputPath,
             self.__import_progress_callback
         )
         success = await task.wait_until_finished()
@@ -73,23 +86,23 @@ class SceneImporter:
             print('Failed to load file: {}'.format(modelPath))
             print(task.get_status())
             print(task.get_error_message())
+            return None
         else:
-            print('Successfully loaded file at path: {}\\{}'.format(os.getcwd(), modelPath))
+            print('Successfully imported file at path: {}'.format(outputPath))
+            return outputPath
 
+    # Get prim at a given path
     def __get_prim(self, primPath):
+        if primPath is None:
+            return None
         stage = omni.usd.get_context().get_stage()
-        # selection = omni.usd.get_context().get_selection()
-        # paths = selection.get_selected_prim_paths()
-
-        # prim = None
-        # for path in paths:
-        #     prim = stage.GetPrimAtPath(path)
-        #     break
-        # return prim
         return stage.GetPrimAtPath(primPath)
 
-    # Add reference to default World prim in stage
-    def __add_model_reference(self, nodeIdx, modelPath):
+    # Build hierarchy of USD based on scene JSON hierarchy
+    def __generate_reference_path(self, nodeIdx):
+        if nodeIdx >= len(self._sceneJSON['nodes']):
+            return None
+
         node = self._sceneJSON['nodes'][nodeIdx]
         modelName = node['name']
 
@@ -103,31 +116,45 @@ class SceneImporter:
         if 'children' in node:
             for childIdx in node['children']:
                 self._sceneJSON['nodes'][childIdx]['parentReferencePath'] = path
-
-        omni.kit.commands.execute(
-            'CreateReference',
-            usd_context=omni.usd.get_context(),
-            path_to=Sdf.Path(path),
-            asset_path=modelPath
-        )
+        
         return path
+
+    # Add reference node to model
+    # Omni can reference a USD or GLTF/GLB file directly
+    def __add_model_reference(self, nodeIdx, modelPath):
+        path = self.__generate_reference_path(nodeIdx)
+
+        if path is not None:
+            omni.kit.commands.execute(
+                'CreateReference',
+                usd_context=omni.usd.get_context(),
+                path_to=Sdf.Path(path),
+                asset_path=modelPath
+            )
+        return path
+
+    def __add_xform_reference(self, nodeIdx):
+        path = self.__generate_reference_path(nodeIdx)
+
+        if path is not None:
+            omni.kit.commands.execute(
+                'CreatePrim',
+                prim_type='Xform',
+                prim_path=path
+            )
 
     async def import_scene_assets(self):
         print('Importing 3D assets for scene')
         nodes = self._sceneJSON['nodes']
-        modelsToLoad = []
         for i in range(len(nodes)):
             node = nodes[i]
             modelPrim = None
             for component in node['components']:
                 if 'uri' in component:
                     modelPath = component['uri']
-                    modelAlreadyDownloaded = os.path.isfile('{}\\{}'.format(os.getcwd(), modelPath))
-                    if not modelAlreadyDownloaded and modelPath not in modelsToLoad:
-                        modelsToLoad.append(modelPath)
-                        await self.__load_model(modelPath)
-
-                    modelReference = self.__add_model_reference(i, modelPath)
+                    await self.__load_model(modelPath)
+                    usdFilePath = await self.__convert_to_usd(modelPath)
+                    modelReference = self.__add_model_reference(i, usdFilePath)
                     modelPrim = self.__get_prim(modelReference)
 
             if modelPrim is not None:
@@ -135,3 +162,6 @@ class SceneImporter:
                 TUtil_SetTranslate(modelPrim, transform['position'])
                 TUtil_SetRotate(modelPrim, transform['rotation'])
                 TUtil_SetScale(modelPrim, transform['scale'])
+            # Add empty transform
+            elif 'children' in node:
+                self.__add_xform_reference(i)
